edacf7c3ff78ce1ddfb4fc07b0f809b658d3ae35
Daniel <daniel.work.2006@gmail.com>
2020-04-21 17:43:33 -0400

    Add User Agree and Start Page

---
+++ adm/useragree.aspx.vb
@@ -0,0 +1,59 @@
+﻿Imports Microsoft.AspNet.Identity
+
+Public Class useragree
+    Inherits System.Web.UI.Page
+
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack() Then
+
+            If LocalAPI.IAgree(Context.User.Identity.GetUserName()) Then
+                Response.RedirectPermanent("~/adm/dashboard.aspx")
+            Else
+                lblTerms.Text = ReadFile(Server.MapPath("~/Legal/ENG/Terms.html"))
+                LocalAPI.SetIAgree(Context.User.Identity.GetUserName())
+                Response.RedirectPermanent("~/adm/dashboard.aspx")
+            End If
+
+        End If
+    End Sub
+
+    Protected Function ReadFile(path As String) As String
+        If Not System.IO.File.Exists(path) Then
+            Return String.Empty
+        End If
+
+        Using sr As New System.IO.StreamReader(path)
+            Return sr.ReadToEnd()
+        End Using
+    End Function
+
+
+    Protected Sub btnDISAGREE_Click(sender As Object, e As EventArgs) Handles btnDISAGREE.Click
+        LocalAPI.SetIDisagree(Membership.GetUser().UserName)
+        Session.Abandon()
+        System.Web.Security.FormsAuthentication.SignOut()
+        Response.RedirectPermanent(LocalAPI.GetHostAppSite())
+    End Sub
+
+    Protected Sub btnAGREE_Click(sender As Object, e As EventArgs) Handles btnAGREE.Click
+        LocalAPI.SetIAgree(Membership.GetUser().UserName)
+        Response.RedirectPermanent("~/ADM/Start.aspx")
+    End Sub
+
+    Protected Sub btnREADLATER_Click(sender As Object, e As EventArgs) Handles btnREADLATER.Click
+        Dim sEmail As String = Membership.GetUser().UserName
+        If LocalAPI.GetIReadLater(sEmail) > 5 Then
+            Session.Abandon()
+            System.Web.Security.FormsAuthentication.SignOut()
+            Response.RedirectPermanent(LocalAPI.GetHostAppSite())
+        Else
+            LocalAPI.SetIReadLater(sEmail)
+            Session("ReadLater") = "1"
+            Response.RedirectPermanent("~/ADM/Start.aspx")
+        End If
+    End Sub
+
+
+End Class
