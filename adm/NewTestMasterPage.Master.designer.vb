44ca53f7775f35ec4da013ea995c18897335caf5
Daniel <daniel.work.2006@gmail.com>
2020-06-01 08:18:36 -0400

    Add New Menu

--- adm/NewTestMasterPage.Master.designer.vb
+++ adm/NewTestMasterPage.Master.designer.vb
@@ -12,7 +12,7 @@ Option Explicit On
 
 
 Partial Public Class NewTestMasterPage
-    
+
     '''<summary>
     '''Head1 control.
     '''</summary>
@@ -21,7 +21,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents Head1 As Global.System.Web.UI.HtmlControls.HtmlHead
-    
+
     '''<summary>
     '''form1 control.
     '''</summary>
@@ -30,7 +30,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
-    
+
     '''<summary>
     '''RadScriptManager1 control.
     '''</summary>
@@ -39,7 +39,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadScriptManager1 As Global.Telerik.Web.UI.RadScriptManager
-    
+
     '''<summary>
     '''RadStyleSheetManager1 control.
     '''</summary>
@@ -48,7 +48,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadStyleSheetManager1 As Global.Telerik.Web.UI.RadStyleSheetManager
-    
+
     '''<summary>
     '''RadPageLayout1 control.
     '''</summary>
@@ -57,7 +57,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadPageLayout1 As Global.Telerik.Web.UI.RadPageLayout
-    
+
     '''<summary>
     '''HeaderPanel control.
     '''</summary>
@@ -66,7 +66,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents HeaderPanel As Global.Telerik.Web.UI.LayoutRow
-    
+
     '''<summary>
     '''FormViewCompany control.
     '''</summary>
@@ -75,7 +75,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents FormViewCompany As Global.System.Web.UI.WebControls.FormView
-    
+
     '''<summary>
     '''lblCompanyName control.
     '''</summary>
@@ -84,7 +84,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblCompanyName As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''lblUserName control.
     '''</summary>
@@ -93,7 +93,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblUserName As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''btnSwitchCompany control.
     '''</summary>
@@ -102,7 +102,25 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents btnSwitchCompany As Global.System.Web.UI.WebControls.LinkButton
-    
+
+    '''<summary>
+    '''Label2 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Label2 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Label3 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
+
     '''<summary>
     '''RadMenu2 control.
     '''</summary>
@@ -110,8 +128,8 @@ Partial Public Class NewTestMasterPage
     '''Auto-generated field.
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
-    Protected WithEvents RadMenu2 As Global.Telerik.Web.UI.RadMenu
-    
+    Protected WithEvents RadMenu2 As Global.Telerik.Web.UI.RadNavigation
+
     '''<summary>
     '''btnAvoidEnterKey control.
     '''</summary>
@@ -120,7 +138,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents btnAvoidEnterKey As Global.System.Web.UI.WebControls.Button
-    
+
     '''<summary>
     '''ContentPlaceHolder1 control.
     '''</summary>
@@ -129,7 +147,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents ContentPlaceHolder1 As Global.System.Web.UI.WebControls.ContentPlaceHolder
-    
+
     '''<summary>
     '''lblVersion control.
     '''</summary>
@@ -138,7 +156,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblVersion As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''Image2 control.
     '''</summary>
@@ -147,7 +165,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents Image2 As Global.System.Web.UI.WebControls.Image
-    
+
     '''<summary>
     '''lnkHelp control.
     '''</summary>
@@ -156,7 +174,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lnkHelp As Global.System.Web.UI.WebControls.HyperLink
-    
+
     '''<summary>
     '''lnkAyuda control.
     '''</summary>
@@ -165,7 +183,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lnkAyuda As Global.System.Web.UI.WebControls.HyperLink
-    
+
     '''<summary>
     '''Label11 control.
     '''</summary>
@@ -174,7 +192,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents Label11 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''HyperLink1 control.
     '''</summary>
@@ -183,7 +201,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents HyperLink1 As Global.System.Web.UI.WebControls.HyperLink
-    
+
     '''<summary>
     '''Label1 control.
     '''</summary>
@@ -192,7 +210,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''lblPageTitle control.
     '''</summary>
@@ -201,7 +219,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblPageTitle As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''RadToolTipSwitchCompany control.
     '''</summary>
@@ -210,7 +228,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadToolTipSwitchCompany As Global.Telerik.Web.UI.RadToolTip
-    
+
     '''<summary>
     '''cboCompany control.
     '''</summary>
@@ -219,7 +237,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents cboCompany As Global.Telerik.Web.UI.RadComboBox
-    
+
     '''<summary>
     '''chkSetAsDefault control.
     '''</summary>
@@ -228,7 +246,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents chkSetAsDefault As Global.Telerik.Web.UI.RadCheckBox
-    
+
     '''<summary>
     '''btnSwitchCompanyConfirm control.
     '''</summary>
@@ -237,7 +255,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents btnSwitchCompanyConfirm As Global.System.Web.UI.WebControls.LinkButton
-    
+
     '''<summary>
     '''SqlDataSourceCompany control.
     '''</summary>
@@ -246,7 +264,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents SqlDataSourceCompany As Global.System.Web.UI.WebControls.SqlDataSource
-    
+
     '''<summary>
     '''SqlDataSourceCompanyLogo control.
     '''</summary>
@@ -255,7 +273,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents SqlDataSourceCompanyLogo As Global.System.Web.UI.WebControls.SqlDataSource
-    
+
     '''<summary>
     '''lblCompanyId control.
     '''</summary>
@@ -264,7 +282,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblCompanyId As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''UpdatePanel1 control.
     '''</summary>
@@ -273,7 +291,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents UpdatePanel1 As Global.System.Web.UI.UpdatePanel
-    
+
     '''<summary>
     '''RadNotificationWarning control.
     '''</summary>
@@ -282,7 +300,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadNotificationWarning As Global.Telerik.Web.UI.RadNotification
-    
+
     '''<summary>
     '''RadNotificationError control.
     '''</summary>
@@ -291,7 +309,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadNotificationError As Global.Telerik.Web.UI.RadNotification
-    
+
     '''<summary>
     '''lblEmployeeId control.
     '''</summary>
@@ -300,7 +318,7 @@ Partial Public Class NewTestMasterPage
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lblEmployeeId As Global.System.Web.UI.WebControls.Label
-    
+
     '''<summary>
     '''lblEmployeeEmail control.
     '''</summary>
