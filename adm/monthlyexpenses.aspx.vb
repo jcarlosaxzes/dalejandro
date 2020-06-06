188905599f210db761f87294b23d62a44f4e9900
Juan Carlos <jcarlos@axzes.com>
2020-06-06 19:14:50 -0400

    incluide Company Multiplier menu

--- adm/monthlyexpenses.aspx.vb
+++ adm/monthlyexpenses.aspx.vb
@@ -31,4 +31,8 @@ Public Class monthlyexpenses
         RadHtmlChartYearly.Visible = Not RadHtmlChartMonthly.Visible
         FloatedTilesListView.DataBind()
     End Sub
+
+    Private Sub SqlDataSource1_Selecting(sender As Object, e As SqlDataSourceSelectingEventArgs) Handles SqlDataSource1.Selecting
+        Dim e1 As String = e.Command.Parameters(0).Value
+    End Sub
 End Class
