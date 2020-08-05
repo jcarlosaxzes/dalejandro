386e59f4d77d9d8b469c5d36dc9d12728d1cf921
Juan Carlos <jcarlos@axzes.com>
2020-08-05 15:53:03 -0400

    reviw dashb

--- adm/Default.aspx.vb
+++ adm/Default.aspx.vb
@@ -34,7 +34,7 @@ Public Class _Default1
                 RadGridClients.DataBind()
                 RadGridSubConsultants.DataBind()
 
-                RadBarcode1.Text = "https://app.pasconcept.com/e2103445_8a47_49ff_808e_6008c0fe13a1/smarth/salesstatus.aspx?GuiId=" & LocalAPI.GetCompanyGUID(lblCompanyId.Text)
+                RadBarcode1.Text = LocalAPI.GetHostAppSite() & "/e2103445_8a47_49ff_808e_6008c0fe13a1/smarth/salesstatus.aspx?GuiId=" & LocalAPI.GetCompanyGUID(lblCompanyId.Text)
 
 
             End If
