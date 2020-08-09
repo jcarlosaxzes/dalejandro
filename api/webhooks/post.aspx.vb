3f1910bbf93e508579565d1e9fdd25dc84a4a625
Juan Carlos <jcarlos@axzes.com>
2020-08-09 12:33:44 -0400

    weebhook (2)

--- api/webhooks/post.aspx.vb
+++ api/webhooks/post.aspx.vb
@@ -11,22 +11,24 @@ Public Class post
                     Dim requestHeaders = HttpContext.Current.Request.Headers
                     Dim authToken = requestHeaders("Authorization")
                     '[WebHook URL] "https://app.pasconcept.com/api/webhooks/post"
-                    '[Authorization Token] = "Bearer 7497EE20-6811-4405-A2EE-471A8BFE3682"
+                    '[Authorization Token] = "Bearer D1532BCE-BBF3-44BD-88C6-34D4151844B0"
                     '[HttpMethod] = "POST"
 
                     Select Case authToken
-                        Case "D1532BCE-BBF3-44BD-88C6-34D4151844B0"
+                        Case "Bearer D1532BCE-BBF3-44BD-88C6-34D4151844B0"
                             'EmissionRecurrenceEmails
                             Task.Run(Function() LocalAPI.EmissionRecurrenceEmails())
+                            Response.StatusCode = 200
                         Case Else
                             Response.StatusCode = 401
-                            Response.End()
-                            Return
                     End Select
+                Else
+                    Response.StatusCode = 401
                 End If
+
             End If
         Catch ex As Exception
-
+            Response.Write(ex.Message)
         End Try
     End Sub
 
