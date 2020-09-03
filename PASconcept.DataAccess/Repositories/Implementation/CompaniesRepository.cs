4ef1a20d2191054f216432919494527062e9c6aa
jcurbelo <curbelorobin@gmail.com>
2020-09-03 18:40:06 -0400

    Configured core projects and added pilot functionality into login page

---
+++ PASconcept.DataAccess/Repositories/Implementation/CompaniesRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using PASconcept.DataAccess.Repositories.Abstract;
+
+namespace PASconcept.DataAccess.Repositories.Implementation
+{
+    public class CompaniesRepository : ICompaniesRepository
+    {
+        private readonly IBaseDapperRepository _baseDapperRepository;
+
+        public CompaniesRepository(
+            IBaseDapperRepository baseDapperRepository)
+        {
+            this._baseDapperRepository = baseDapperRepository;
+        }
+
+        public int GetDefaultCompanyId(string userEmail)
+        {
+            var pars = new DynamicParameters();
+            pars.Add("@Email", userEmail);
+
+            const string spName = "dbo.Company_default_SELECT";
+
+            return this._baseDapperRepository.QuerySingleOrDefault<int>(spName, pars, CommandType.StoredProcedure);
+        }
+    }
+}
---
+++ PASconcept.Domain/Entities/EntityViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace PASconcept.Domain.Entities
+{
+    /// <summary>
+    ///     Contains the common property of every VM.
+    ///     The type of the PK is int.
+    /// </summary>
+    public abstract class EntityViewModel<TKey> : IEntityViewModel<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+
+    /// <summary>
+    ///     Base class that encapsulates all common VM properties
+    /// </summary>
+    public class EntityViewModel : EntityViewModel<int>
+    {
+    }
+
+    public interface IEntityViewModel<TKey>
+    {
+        TKey Id { get; set; }
+    }
+
+    public interface IEntityViewModel : IEntityViewModel<int>
+    {
+    }
+}
