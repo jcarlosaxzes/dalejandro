2a924d748f3c8f5297ee9cac65630dc0ea6ba8cb
Juan Carlos <jcarlos@axzes.com>
2020-11-11 17:14:04 -0500

    RadAjaxLoadingPanel1 with Logo-Animation250x250.gif

--- adm/jobs.aspx.designer.vb
+++ adm/jobs.aspx.designer.vb
@@ -32,6 +32,15 @@ Partial Public Class jobs
     Protected WithEvents RadAjaxLoadingPanel1 As Global.Telerik.Web.UI.RadAjaxLoadingPanel
 
     '''<summary>
+    '''Label1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
     '''RadCodeBlock control.
     '''</summary>
     '''<remarks>
