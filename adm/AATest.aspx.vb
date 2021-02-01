4afbd2649c06d4cd58da1790308eddbce096eb6b
developer <dluque.daniel@gmail.com>
2021-02-01 13:36:45 +0000

    Ebillity Time Entries Integration

---
+++ adm/AATest.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports Telerik.Web.UI
+
+Public Class AATest
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            lblCompanyId.Text = Session("companyId")
+        End If
+
+
+        RadWindowManager1.EnableViewState = False
+
+    End Sub
+
+#Region "TimeEntries"
+
+    Private Sub btnGetTimeEntries_Click(sender As Object, e As EventArgs) Handles btnGetTimeEntries.Click
+        System.Threading.Tasks.Task.Run(Function() EbillityApi.GetTimeEntriesAsync(lblCompanyId.Text))
+    End Sub
+
+
+#End Region
+
+
+
+End Class
