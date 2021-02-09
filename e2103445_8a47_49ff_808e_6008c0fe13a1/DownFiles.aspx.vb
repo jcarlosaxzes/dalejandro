1479fa6c2c861350af6ceb0cf1c80b0db4c2a726
developer <dluque.daniel@gmail.com>
2021-02-09 20:43:07 +0000

    Add unique downloadFile Pages

---
+++ e2103445_8a47_49ff_808e_6008c0fe13a1/DownFiles.aspx.vb
@@ -0,0 +1,35 @@
+﻿Public Class DownFiles
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+
+        Dim guid = Request.QueryString("guid")
+        If Not String.IsNullOrEmpty(guid) Then
+            Dim recor = LocalAPI.GetRecordFromQuery($"select * FROM [dbo].[Azure_Uploads] where [guid] = '{guid}'")
+            If Not IsNothing(recor) AndAlso recor.ContainsKey("KeyName") Then
+                Dim response As HttpResponse = Page.Response
+
+                response.Clear()
+                response.Buffer = True
+
+                response.AddHeader("Content-Description", "File Transfer")
+                response.AddHeader("Content-Type", recor("ContentType"))
+                response.AddHeader("Content-Disposition", $"attachment;filename=""{recor("OriginalFileName")}""")
+
+                Dim blob = AzureStorageApi.GetCloudBlockBlob(recor("KeyName"))
+                blob.DownloadToStream(response.OutputStream)
+                response.[End]()
+            End If
+        Else
+            Response.Redirect("~/adm/default")
+        End If
+
+
+
+
+
+
+    End Sub
+
+End Class
